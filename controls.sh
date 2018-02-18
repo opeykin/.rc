931788b8f67ba59e8274c4df6623f4841a29eab3
alexander.opeykin <alexander.opeykin@gmail.com>
2018-02-18 00:23:44 +0000

    add color aliases from min

--- controls.sh
+++ controls.sh
@@ -2,9 +2,14 @@ bind '"\eu":"cd ..\C-m"'
 alias sc='xclip -selection c'
 alias gc='xclip -selection clipboard -o'
 
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
 alias gdb='gdb --silent'
 
-up () { local p= i=${1:-1}; while (( i-- )); do p+=../; done; cd "$p[" && pwd; }
+u() { local p= i=${1:-1}; while (( i-- )); do p+=../; done; cd "$p[" && pwd; }
 md () { mkdir -p "$@" && cd "$@"; }
 
 alias set-ttl="sudo iptables -t mangle -A POSTROUTING -o wlan0 -j TTL --ttl-set 65"
@@ -19,7 +24,6 @@ function mosh ()
 {
  /usr/local/bin/mosh -6 $@ -- tmux attach -d
 }
-
 
 LAST_FIND_RESULTS=/tmp/last_find_results
 LAST_FIND_ERRORS=/tmp/last_find_errors
